e62077749717c2d8c4b8e11dd04bd0042a04998e
Mark-a-obrien <mark.a.obrien00@gmail.com>
2024-03-20 13:48:10 +0400

    Fixing Accessibility
    fixed label name for dropdown

--- src/components/dropDown.tsx
+++ src/components/dropDown.tsx
@@ -2,8 +2,8 @@
 const DropDown = ({options, label, name}: {options:Array<{value:string, label:string}>, label:string, name:string}) => {
   return (
     <div className="flex flex-col items-start gap-2 w-full">
-    <label htmlFor="dropdown">{label}</label>
-    <select className="p-2 w-full bg-blue-600 border-4 border-black rounded-lg" name={"dropdown-"+name} id="">
+    <label htmlFor={"dropdown-"+name}>{label}</label>
+    <select className="p-2 w-full bg-blue-600 border-4 border-black rounded-lg" name={"dropdown-"+name} id={"dropdown-"+name}>
       {options.map((option, index) => <option key={index} value={option.value}>{option.label}</option>)}
     </select>
     </div>
--- src/components/quiz.tsx
+++ src/components/quiz.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import Question from "./question";
 import Answer from "./answer";
 import { MouseEvent } from "react";
